b74eb3d8c47a4a5caa0d450fbd99f9c98c968881
Mamun Billah <kmmchmbillah@gmail.com>
2026-06-02 18:26:27 +0600

    ddd

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# ৪. 🎯 অটো-সুপারইউজার বা অ্যাডমিন ইউজার তৈরি করা
+if [ "$CREATE_SUPERUSER" ]
+then
+  python manage.py createsuperuser --noinput || true
+fi
